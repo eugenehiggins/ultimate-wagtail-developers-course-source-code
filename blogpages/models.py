49295f12a179e0232e087e53681321bfa9d9f176
Gene Higgins <hi@genehiggins.com>
2024-09-16 17:08:57 -0600

    created blog index and detail models and templates

---
+++ blogpages/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import RichTextField
+from wagtail.models import Page
+
+
+class BlogIndex(Page):
+
+    template = 'blogpages/blog_index_page.html'
+
+
+    subtitle = models.CharField(max_length=100, blank=True)
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('subtitle'),
+        FieldPanel('body'),
+    ]
+
+
+class BlogDetail(Page):
+    subtitle = models.CharField(max_length=100, blank=True)
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('subtitle'),
+        FieldPanel('body'),
+    ]
